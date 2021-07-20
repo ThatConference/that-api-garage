13b6f7f1aba416428472dddebab0d818e4ae19bb
Brett Slaski <brett@thatconference.com>
2021-07-20 12:37:06 -0500

    Supports new type RegistrationProfile, adds new type RegistrationOrderAllocation

---
+++ src/graphql/resolvers/queries/RegistrationOrderAllocation.js
@@ -0,0 +1,38 @@
+import fetchAllocationsQuestionsLink from '../../../lib/that/fetchAllocationsQuestionsLink';
+
+export const fieldResolvers = {
+  RegistrationOrderAllocation: {
+    event: ({ event: id }) => ({ id }),
+    orderId: ({ order: orderId }) => orderId,
+    product: ({ product: productId }, __, { dataSources: { productLoader } }) =>
+      productLoader.load(productId),
+    allocatedTo: ({ allocatedTo }, __, { dataSources: { memberLoader } }) => {
+      if (!allocatedTo) return null;
+      return memberLoader.load(allocatedTo);
+    },
+    purchasedBy: ({ purchasedBy }, __, { dataSources: { memberLoader } }) =>
+      memberLoader.load(purchasedBy),
+    isAllocated: ({ isAllocated, allocatedTo }) =>
+      isAllocated && allocatedTo?.length > 4,
+    questionsLink: (
+      { event: eventId, product: productId, id: orderAllocationId },
+      __,
+      { dataSources: { firestore } },
+    ) =>
+      fetchAllocationsQuestionsLink({
+        eventId,
+        productId,
+        firestore,
+        orderAllocationId,
+      }),
+    hasCheckedIn: ({ checkedInAt }) => {
+      let isCheckedIn = false;
+      if (checkedInAt) isCheckedIn = true;
+      return isCheckedIn;
+    },
+    checkedInBy: ({ checkedInBy }, __, { dataSources: { memberLoader } }) => {
+      if (!checkedInBy) return null;
+      return memberLoader.load(checkedInBy);
+    },
+  },
+};
