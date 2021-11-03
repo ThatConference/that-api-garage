811708094f1d838fcfce63d7c3546d6104d6fae1
Brett Slaski <brett@thatconference.com>
2021-11-03 17:25:45 -0500

    remove activies check to write tshirt/hoodie size

--- src/graphql/resolvers/mutations/meOrderAllocation.js
+++ src/graphql/resolvers/mutations/meOrderAllocation.js
@@ -2,7 +2,6 @@ import debug from 'debug';
 import { AllocationError } from '../../../lib/errors';
 import memberStore from '../../../dataSources/cloudFirestore/member';
 import orderStore from '../../../dataSources/cloudFirestore/order';
-import productStore from '../../../dataSources/cloudFirestore/product';
 
 const dlog = debug('that:api:garage:mutation:meOrderAllocation');
 
@@ -100,16 +99,22 @@ export const fieldResolvers = {
         result.message = `Allocation Id ${orderAllocationId} is not part of provided order`;
         return result;
       }
-      const product = await productStore(firestore).get(
-        orderAllocation.product,
-      );
-      const { eventActivities } = product;
-      if (responses.tshirtSize && !eventActivities.includes('T_SHIRT')) {
-        result.message = `Product ${product.name} doesn't include a t-shirt`;
-      } else if (responses.hoodieSize && !eventActivities.includes('HOODIE')) {
-        result.message = `Product ${product.name} doesn't include a Hoodie`;
-      }
-      if (result.message !== '') return result;
+      /*
+       * The following block of code will validate that an activity exists
+       * to set a t-shirt or hoodie size on an allocation. It was decided
+       * to not do this check, though we'll keep this code here if we wish to
+       * add the logic back
+       */
+      // const product = await productStore(firestore).get(
+      //   orderAllocation.product,
+      // );
+      // const { eventActivities } = product;
+      // if (responses.tshirtSize && !eventActivities.includes('T_SHIRT')) {
+      //   result.message = `Product ${product.name} doesn't include a t-shirt`;
+      // } else if (responses.hoodieSize && !eventActivities.includes('HOODIE')) {
+      //   result.message = `Product ${product.name} doesn't include a Hoodie`;
+      // }
+      // if (result.message !== '') return result;
 
       const oaUpdate = {
         tshirtSize: responses.tshirtSize || null,
