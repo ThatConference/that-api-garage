584758b5c7c0ef1c4d6f3c7ab9242755fcad553f
Brett Slaski <brett@thatconference.com>
2022-01-27 11:04:15 -0600

    Add GraphCDN event emitter for purging cache

--- src/graphql/resolvers/mutations/productCreate.js
+++ src/graphql/resolvers/mutations/productCreate.js
@@ -1,73 +1,170 @@
 import debug from 'debug';
 
 import productStore from '../../../dataSources/cloudFirestore/product';
+import constants from '../../../constants';
 
 const dlog = debug('that:api:garage:mutation:ProductCreate');
 
+function sendGraphCdnEvent({ graphCdnEvents, eventId }) {
+  graphCdnEvents.emit(constants.GRAPHCDN.EVENT_NAME.CREATED_PRODUCT, {
+    eventId,
+  });
+}
+
 export const fieldResolvers = {
   ProductCreate: {
-    ticket: (_, { ticket }, { dataSources: { firestore }, user }) => {
+    ticket: (
+      _,
+      { ticket },
+      {
+        dataSources: {
+          firestore,
+          events: { graphCdnEvents },
+        },
+        user,
+      },
+    ) => {
       dlog('create ticket called');
       const newProduct = ticket;
       newProduct.type = 'TICKET';
-      return productStore(firestore).create({
-        newProduct,
-        userId: user.sub,
-      });
+      return productStore(firestore)
+        .create({
+          newProduct,
+          userId: user.sub,
+        })
+        .then(result => {
+          sendGraphCdnEvent({ graphCdnEvents, eventId: result.eventId });
+          return result;
+        });
     },
 
-    membership: (_, { membership }, { dataSources: { firestore }, user }) => {
+    membership: (
+      _,
+      { membership },
+      {
+        dataSources: {
+          firestore,
+          events: { graphCdnEvents },
+        },
+        user,
+      },
+    ) => {
       dlog('create membership called');
       const newProduct = membership;
       newProduct.type = 'MEMBERSHIP';
-      return productStore(firestore).create({
-        newProduct,
-        userId: user.sub,
-      });
+      return productStore(firestore)
+        .create({
+          newProduct,
+          userId: user.sub,
+        })
+        .then(result => {
+          sendGraphCdnEvent({ graphCdnEvents, eventId: result.eventId });
+          return result;
+        });
     },
 
-    partnership: (_, { partnership }, { dataSources: { firestore }, user }) => {
+    partnership: (
+      _,
+      { partnership },
+      {
+        dataSources: {
+          firestore,
+          events: { graphCdnEvents },
+        },
+        user,
+      },
+    ) => {
       dlog('create partnership called');
       const newProduct = partnership;
       newProduct.type = 'PARTNERSHIP';
-      return productStore(firestore).create({
-        newProduct,
-        userId: user.sub,
-      });
+      return productStore(firestore)
+        .create({
+          newProduct,
+          userId: user.sub,
+        })
+        .then(result => {
+          sendGraphCdnEvent({ graphCdnEvents, eventId: result.eventId });
+          return result;
+        });
     },
 
     coupon: () => {
       throw new Error('Not implemented yet');
     },
 
-    food: (_, { food }, { dataSources: { firestore }, user }) => {
+    food: (
+      _,
+      { food },
+      {
+        dataSources: {
+          firestore,
+          events: { graphCdnEvents },
+        },
+        user,
+      },
+    ) => {
       dlog('create food called');
       const newProduct = food;
       newProduct.type = 'FOOD';
-      return productStore(firestore).create({
-        newProduct,
-        userId: user.sub,
-      });
+      return productStore(firestore)
+        .create({
+          newProduct,
+          userId: user.sub,
+        })
+        .then(result => {
+          sendGraphCdnEvent({ graphCdnEvents, eventId: result.eventId });
+          return result;
+        });
     },
 
-    training: (_, { training }, { dataSources: { firestore }, user }) => {
+    training: (
+      _,
+      { training },
+      {
+        dataSources: {
+          firestore,
+          events: { graphCdnEvents },
+        },
+        user,
+      },
+    ) => {
       dlog('create training called');
       const newProduct = training;
       newProduct.type = 'TRAINING';
-      return productStore(firestore).create({
-        newProduct,
-        userId: user.sub,
-      });
+      return productStore(firestore)
+        .create({
+          newProduct,
+          userId: user.sub,
+        })
+        .then(result => {
+          sendGraphCdnEvent({ graphCdnEvents, eventId: result.eventId });
+          return result;
+        });
     },
 
-    family: (_, { family }, { dataSources: { firestore }, user }) => {
+    family: (
+      _,
+      { family },
+      {
+        dataSources: {
+          firestore,
+          events: { graphCdnEvents },
+        },
+        user,
+      },
+    ) => {
       dlog('create family called');
       const newProduct = family;
       newProduct.type = 'FAMILY';
-      return productStore(firestore).create({
-        newProduct,
-        userId: user.sub,
-      });
+      return productStore(firestore)
+        .create({
+          newProduct,
+          userId: user.sub,
+        })
+        .then(result => {
+          sendGraphCdnEvent({ graphCdnEvents, eventId: result.eventId });
+          return result;
+        });
     },
   },
 };
--- src/graphql/resolvers/mutations/productUpdate.js
+++ src/graphql/resolvers/mutations/productUpdate.js
@@ -1,87 +1,166 @@
 import debug from 'debug';
 
 import productStore from '../../../dataSources/cloudFirestore/product';
+import constants from '../../../constants';
 
 const dlog = debug('that:api:garage:mutation:ProductUpdate');
+
+function sendGraphCdnEvent({ graphCdnEvents, productId }) {
+  graphCdnEvents.emit(
+    constants.GRAPHCDN.EVENT_NAME.PURGE,
+    constants.GRAPHCDN.PURGE.PRODUCT,
+    productId,
+  );
+}
 
 export const fieldResolvers = {
   ProductUpdate: {
     ticket: (
       { productId },
       { ticket },
-      { dataSources: { firestore }, user },
+      {
+        dataSources: {
+          firestore,
+          events: { graphCdnEvents },
+        },
+        user,
+      },
     ) => {
       dlog('update ticket called');
-      return productStore(firestore).update({
-        productId,
-        newProduct: ticket,
-        userId: user.sub,
-      });
+      return productStore(firestore)
+        .update({
+          productId,
+          newProduct: ticket,
+          userId: user.sub,
+        })
+        .then(result => {
+          sendGraphCdnEvent({ graphCdnEvents, productId });
+          return result;
+        });
     },
 
     membership: (
       { productId },
       { membership },
-      { dataSources: { firestore }, user },
+      {
+        dataSources: {
+          firestore,
+          events: { graphCdnEvents },
+        },
+        user,
+      },
     ) => {
       dlog('update membership called');
-      return productStore(firestore).update({
-        productId,
-        newProduct: membership,
-        userId: user.sub,
-      });
+      return productStore(firestore)
+        .update({
+          productId,
+          newProduct: membership,
+          userId: user.sub,
+        })
+        .then(result => {
+          sendGraphCdnEvent({ graphCdnEvents, productId });
+          return result;
+        });
     },
 
     partnership: (
       { productId },
       { partnership },
-      { dataSources: { firestore }, user },
+      {
+        dataSources: {
+          firestore,
+          events: { graphCdnEvents },
+        },
+        user,
+      },
     ) => {
       dlog('update partnership called');
-      return productStore(firestore).update({
-        productId,
-        newProduct: partnership,
-        userId: user.sub,
-      });
+      return productStore(firestore)
+        .update({
+          productId,
+          newProduct: partnership,
+          userId: user.sub,
+        })
+        .then(result => {
+          sendGraphCdnEvent({ graphCdnEvents, productId });
+          return result;
+        });
     },
 
     coupon: () => {
       throw new Error('Not implemented yet');
     },
 
-    food: ({ productId }, { food }, { dataSources: { firestore }, user }) => {
+    food: (
+      { productId },
+      { food },
+      {
+        dataSources: {
+          firestore,
+          events: { graphCdnEvents },
+        },
+        user,
+      },
+    ) => {
       dlog('update food called');
-      return productStore(firestore).update({
-        productId,
-        newProduct: food,
-        userId: user.sub,
-      });
+      return productStore(firestore)
+        .update({
+          productId,
+          newProduct: food,
+          userId: user.sub,
+        })
+        .then(result => {
+          sendGraphCdnEvent({ graphCdnEvents, productId });
+          return result;
+        });
     },
 
     training: (
       { productId },
       { training },
-      { dataSources: { firestore }, user },
+      {
+        dataSources: {
+          firestore,
+          events: { graphCdnEvents },
+        },
+        user,
+      },
     ) => {
       dlog('update training called');
-      return productStore(firestore).update({
-        productId,
-        newProduct: training,
-        userId: user.sub,
-      });
+      return productStore(firestore)
+        .update({
+          productId,
+          newProduct: training,
+          userId: user.sub,
+        })
+        .then(result => {
+          sendGraphCdnEvent({ graphCdnEvents, productId });
+          return result;
+        });
     },
 
     family: (
       { productId },
       { family },
-      { dataSources: { firestore }, user },
+      {
+        dataSources: {
+          firestore,
+          events: { graphCdnEvents },
+        },
+        user,
+      },
     ) => {
       dlog('update family called');
-      return productStore(firestore).update({
-        productId,
-        newProduct: family,
-        userId: user.sub,
-      });
+      return productStore(firestore)
+        .update({
+          productId,
+          newProduct: family,
+          userId: user.sub,
+        })
+        .then(result => {
+          sendGraphCdnEvent({ graphCdnEvents, productId });
+          return result;
+        });
     },
   },
 };
