2db5f235e2ce2d17ccaf637101ca100626634667
Brett Slaski <brett@thatconference.com>
2022-02-25 14:26:21 -0600

    fix typo

--- src/graphql/resolvers/queries/index.js
+++ src/graphql/resolvers/queries/index.js
@@ -16,7 +16,7 @@ import { fieldResolvers as assetFields } from './asset';
 import { fieldResolvers as entityFields } from './entity';
 import { fieldResolvers as meAssetsFields } from './meAssets';
 import { fieldResolvers as registrationOrderAllocationFields } from './RegistrationOrderAllocation';
-import { fieldResolvers as EventAdminFields } from './eventAdmin';
+import { fieldResolvers as eventAdminFields } from './eventAdmin';
 
 export default {
   ...root,
@@ -39,5 +39,5 @@ export const fieldResolvers = {
   ...entityFields,
   ...meAssetsFields,
   ...registrationOrderAllocationFields,
-  ...EventAdminFields,
+  ...eventAdminFields,
 };
