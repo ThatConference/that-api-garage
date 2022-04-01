c90b5a59d5ac05852fbabe80a1c484e310ee644b
Brett Slaski <brett@thatconference.com>
2022-04-01 15:49:10 -0500

    Add mutations for order allocation cancel and refund

--- src/graphql/resolvers/mutations/orderAllocation.js
+++ src/graphql/resolvers/mutations/orderAllocation.js
@@ -1,4 +1,5 @@
 import debug from 'debug';
+import orderStore from '../../../dataSources/cloudFirestore/order';
 
 const dlog = debug(`that:api:garage:mutation:OrderAllocationMutation`);
 
@@ -9,9 +10,43 @@ export const fieldResolvers = {
       dlog('allocateTo called');
       throw new Error('Not Implemented Yet');
     },
-    setRefunded: () => {
-      dlog('setRefunded called');
-      throw new Error('Not Implemented Yet');
+    setRefunded: (
+      { orderAllocationId },
+      __,
+      { dataSources: { firestore }, user },
+    ) => {
+      dlog('setRefunded called on %s', orderAllocationId);
+      const updateAllocation = {
+        allocatedTo: null,
+        isAllocated: false,
+        status: 'REFUNDED',
+      };
+      return orderStore(firestore)
+        .updateOrderAllocation({
+          orderAllocationId,
+          updateAllocation,
+          user,
+        })
+        .then(({ id }) => orderStore(firestore).getOrderAllocation(id));
+    },
+    setCancelled: (
+      { orderAllocationId },
+      __,
+      { dataSources: { firestore }, user },
+    ) => {
+      dlog('setCancelled called on %s', orderAllocationId);
+      const updateAllocation = {
+        allocatedTo: null,
+        isAllocated: false,
+        status: 'CANCELLED',
+      };
+      return orderStore(firestore)
+        .updateOrderAllocation({
+          orderAllocationId,
+          updateAllocation,
+          user,
+        })
+        .then(({ id }) => orderStore(firestore).getOrderAllocation(id));
     },
   },
 };
