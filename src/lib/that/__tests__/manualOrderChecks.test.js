75ba383ae92769b5ed1a0249dd46ee0d82d135e8
Brett Slaski <brett@thatconference.com>
2022-09-19 10:16:12 -0500

    Validate product dates on manual orders

--- src/lib/that/__tests__/manualOrderChecks.test.js
+++ src/lib/that/__tests__/manualOrderChecks.test.js
@@ -1,33 +1,34 @@
-import product from '../../../dataSources/cloudFirestore/product';
 import manualOrderChecks from '../manualOrderChecks';
 
-const productsObj = [
-  {
-    id: 'productId_0',
-    name: 'test ticket product',
-    eventId: 'test_eventId',
-    isEnabled: true,
-    isClaimable: true,
-  },
-  {
-    id: 'productId_1',
-    name: 'test ticket product',
-    eventId: 'test_eventId',
-    isEnabled: true,
-  },
-];
 const productIdsObj = ['productId_0', 'productId_1'];
 let eventId = 'test_eventId';
 
 describe('claimORderChecks tests', () => {
+  let productsObj;
   let products;
   let productIds;
 
   beforeEach(() => {
+    productsObj = [
+      {
+        id: 'productId_0',
+        name: 'test ticket product',
+        eventId: 'test_eventId',
+        isEnabled: true,
+        isClaimable: true,
+      },
+      {
+        id: 'productId_1',
+        name: 'test ticket product',
+        eventId: 'test_eventId',
+        isEnabled: true,
+      },
+    ];
     products = [...productsObj];
     productIds = [...productIdsObj];
   });
   afterEach(() => {
+    productsObj = null;
     products = null;
     productIds = null;
     eventId = 'test_eventId';
@@ -80,6 +81,69 @@ describe('claimORderChecks tests', () => {
         expect(result[0]).toBe(
           `'MEMBERSHIP' type products cannot be added to manual orders`,
         );
+      });
+    });
+    describe('Products with dates must be within those dates', () => {
+      const epoch = new Date().getTime();
+      const diff = 3600000;
+      let result;
+      // onSaleFrom: new Date(epoch - 60000),
+      // onSaleUntil: new Date(epoch + 60000),
+      it('Having no product dates defined succeed', () => {
+        result = manualOrderChecks({ products, productIds, eventId });
+        expect(result?.length).toBe(0);
+      });
+      it('Having only onSaleFrom before now succeeds', () => {
+        const mutate = { ...products[0] };
+        mutate.onSaleFrom = new Date(epoch - diff);
+        products[0] = mutate;
+        result = manualOrderChecks({ products, productIds, eventId });
+        expect(result?.length).toBe(0);
+      });
+      it('Having only onSaleFrom after now errors', () => {
+        const mutate = { ...products[0] };
+        mutate.onSaleFrom = new Date(epoch + diff);
+        products[0] = mutate;
+        [result] = manualOrderChecks({ products, productIds, eventId });
+        expect(result).toBe('Product not available for sale (date)');
+      });
+      it('Having only onSale until after now succeeds', () => {
+        const mutate = { ...products[0] };
+        mutate.onSaleUntil = new Date(epoch + diff);
+        products[0] = mutate;
+        result = manualOrderChecks({ products, productIds, eventId });
+        expect(result?.length).toBe(0);
+      });
+      it('Having only onSaleUntil before now errors', () => {
+        const mutate = { ...products[0] };
+        mutate.onSaleUntil = new Date(epoch - diff);
+        products[0] = mutate;
+        [result] = manualOrderChecks({ products, productIds, eventId });
+        expect(result).toBe('Product not available for sale (date)');
+      });
+      it('Having both product dates in range succeeds', () => {
+        const mutate = { ...products[0] };
+        mutate.onSaleFrom = new Date(epoch - diff);
+        mutate.onSaleUntil = new Date(epoch + diff);
+        products[0] = mutate;
+        result = manualOrderChecks({ products, productIds, eventId });
+        expect(result?.length).toBe(0);
+      });
+      it('Having both dates and onSaleUntil before now, errors (from ok)', () => {
+        const mutate = { ...products[0] };
+        mutate.onSaleFrom = new Date(epoch - diff);
+        mutate.onSaleUntil = new Date(epoch - diff);
+        products[0] = mutate;
+        [result] = manualOrderChecks({ products, productIds, eventId });
+        expect(result).toBe('Product not available for sale (date)');
+      });
+      it('Having both dates and onSalefrom after now, errors (until ok)', () => {
+        const mutate = { ...products[0] };
+        mutate.onSaleFrom = new Date(epoch + diff);
+        mutate.onSaleUntil = new Date(epoch + diff);
+        products[0] = mutate;
+        [result] = manualOrderChecks({ products, productIds, eventId });
+        expect(result).toBe('Product not available for sale (date)');
       });
     });
   });
